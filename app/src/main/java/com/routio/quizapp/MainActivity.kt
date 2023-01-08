2ab76fc81ec7edc02ef73eec42d0f39d394740d3
Deepak <der9078@nyp.org>
2023-01-08 17:36:14 -0500

    Initial commit

---
+++ app/src/main/java/com/routio/quizapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.routio.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnStart : Button = findViewById(R.id.buttonStart)
+        val editText : EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener{
+            if (editText.text.isNotEmpty()) {
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }else {
+                Toast.makeText(this,"Please enter your name", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
